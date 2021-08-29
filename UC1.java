9467103d8ce93ff9421e496c774c730daf2a7627
Tejashree Kharmate <tejashree.kharmate@morningstar.com>
2021-08-29 14:41:34 +0530

    First usecase

---
+++ UC1.java
@@ -0,0 +1,17 @@
+public class EmpWageBuilderUC1 {
+	
+	  public static void main(String[] args) {
+		  
+		// class members(constant)
+		int IS_FULL_TIME = 1;
+		
+		//computation
+		double empCheck = Math.floor(Math.random()*10) % 2;
+		
+		if (empCheck == IS_FULL_TIME)
+		    System.out.println("Employee is Present");
+		else
+		    System.out.println("Employee is Absent");
+	}
+
+}
